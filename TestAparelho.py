01164c5f8b604fe4f09971056e3fd5c1e6d389d8
Psychodrain <psychodrain@hotmail.com>
2011-02-18 20:53:58 -0200

    Exclusão dos modulos TestLoja.py e Loja.py Exclusão do objeto Termo transformando-o em um dicionario dentro de Aparelho

--- TestAparelho.py
+++ TestAparelho.py
@@ -1,20 +1,20 @@
 import unittest
 from Aparelho import Aparelho
-from Termo import Termo
 from Cliente import Cliente
 
 class TestAparelho(unittest.TestCase):
 
     def testCriacaodeAparelho(self):
-        termo = Termo()
         cliente = Cliente()
-        self.aparelho = Aparelho("sony", "vaio", 1040, termo, cliente)
+        self.aparelho = Aparelho("sony", "vaio", 1040, {}, cliente)
         self.assertEqual(self.aparelho.marca, 'sony')
         self.assertEqual(self.aparelho.modelo, 'vaio')
         self.assertEqual(self.aparelho.num_serie, 1040)
-        self.assertEqual(self.aparelho.termo_garantia, termo)
+        self.assertEqual(self.aparelho.termo_garantia, {})
         self.assertEqual(self.aparelho.cliente, cliente)
-            
+
+
+                
 
 if __name__== "__main__":
     unittest.main()
